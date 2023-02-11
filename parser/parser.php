64e12a063edf52135470ebca0fdbd35ac97175e8
KolouchPetr <kolouch.petr4@email.cz>
2023-02-11 15:03:36 +0100

    now deleting comments and blank lines, xml functions added

--- parser/parser.php
+++ parser/parser.php
@@ -2,25 +2,26 @@
 <?php
 ini_set('display_errors', 'stderr');
 
-enum HeaderState {
-	case HEADER;
-	case HEADER_DOT;
-	case HEADER_I;
-	case HEADER_IP;
-	case HEADER_IPP;
-	case HEADER_C;
-	case HEADER_CO;
-	case HEADER_COD;
-	case HEADER_CODE;
-	case HEADER_CODE2;
-	case HEADER_CODE23;
-}
+$HEADER = ".IPPcode23";
+$XMLHEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+$XMLPROGRAMSTART = "<program language=\"IPPcode23\">";
 
 enum ScannerState {
-	case START;
-	case CONSTANT;
-	case IDENTIFIER;
-	case COMMENT;
+	case S_HEADER;
+	case S_HEADER_DOT;
+	case S_HEADER_I;
+	case S_HEADER_IP;
+	case S_HEADER_IPP;
+	case S_HEADER_C;
+	case S_HEADER_CO;
+	case S_HEADER_COD;
+	case S_HEADER_CODE;
+	case S_HEADER_CODE2;
+	case S_HEADER_CODE23;
+	case S_START;
+	case S_CONSTANT;
+	case S_IDENTIFIER;
+	case S_COMMENT;
 }
 
 enum TokenType {
@@ -30,10 +31,14 @@ enum TokenType {
 	case GF;
 	case LF;
 	case TF;
+	case END;
 }
 
 
 class Parser {
+
+
+
 	public function readSTDIN() {
 		$input_data = file_get_contents("php://stdin");
 		return $input_data;
@@ -46,8 +51,30 @@ class Parser {
 }
 
 class Token {
-	function __construct($type, ) {
+	public $type;
+	public $name;
+	public $value;
+	function __construct($type, $name, $value) {
+		$this->type = $type;
+		$this->name = $name;
+		$this->value = $value;
+	}
+}
 
+class XML {
+	public function createStartTag($tagName, $args, $value) {
+		$tag = "<". $tagname . " ";
+		foreach($args as $arg) {
+			$tag .= $arg . " ";
+		}
+		$tag .= ">" . $value;
+
+		return $tag;
+	}
+
+	public function createEndTag($tagName) {
+		$tag = "</" . $tagname . ">";
+		return $tag;
 	}
 }
 
@@ -71,12 +98,60 @@ function printHelp() {
 if($argc == 2) {
 	if(!strcmp($argv[1], "--help") || !strcmp($argv[1], "-help")) {
 		printHelp();
+		exit(0);
 	} else {
 		echo "use -help or --help to print usage";
+		exit(1);
 	} 
-} 
+}
+
 $parser = new Parser();
 $input_data = $parser->readSTDIN();
-echo $input_data;
+$lines = explode("\n", $input_data);
+
+$linesWithoutComments = array_map(function($line) {
+    return preg_replace('/#.*/', '', $line);
+}, $lines);
+
+$noBlankLines = preg_grep('/^[\s]*$/', $linesWithoutComments, PREG_GREP_INVERT);
+
+foreach($noBlankLines as $line) {
+	echo $line;
+	echo "\n";
+}
+
+
+if(strcmp(strtolower($noBlankLines[0]), strtolower($HEADER))) {
+	fwrite(STDERR, "Header is either missing or is incorrect");
+	exit(1);
+}
+
+$instructions = array("MOVE", "CREATEFRAME", "PUSHFRAME", "POPFRAME", "DEFVAR", "CALL", "RETURN","PUSHS", "POPS",
+					"ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "NOT", "INT2CHAR", "STRI2INT",
+					"READ", "WRITE",
+					"CONCAT", "STRLEN", "GETCHAR", "SETCHAR",
+					"TYPE",
+					"LABEL", "JUMP", "JUMPIFEQ", "JUMPIFNEQ", "EXIT",
+					"DPRINT", "BREAK");
+
+$commentRegex = "/\s#.*/";
+$singleArgumentRegex = "";
+$twoArgumentRegex = "";
+$threeArgumentRegex = "";
+
+
+
+
+/* 
+plan
+1. parse input into lines
+2. remove comments from lines
+3. remove empty lines
+3. check line if its one of the possible options (instruction/label, ...)
+4. check if the line makes sense in terms of syntax (undefined variable, redefined variable, scope etc)
+5. if line is valid, add it to the output xml
+*/
+
+
 
 ?>
