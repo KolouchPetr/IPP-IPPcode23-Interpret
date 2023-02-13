13d5c56438ddbd824acde01832e20274cc81cda7
KolouchPetr <kolouch.petr4@email.cz>
2023-02-13 13:41:44 +0100

    parser now generates some broken code

--- parser/parser.php
+++ parser/parser.php
@@ -2,79 +2,149 @@
 <?php
 ini_set('display_errors', 'stderr');
 
-$HEADER = ".IPPcode23";
 $XMLHEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
 $XMLPROGRAMSTART = "<program language=\"IPPcode23\">";
 
-enum ScannerState {
-	case S_HEADER;
-	case S_HEADER_DOT;
-	case S_HEADER_I;
-	case S_HEADER_IP;
-	case S_HEADER_IPP;
-	case S_HEADER_C;
-	case S_HEADER_CO;
-	case S_HEADER_COD;
-	case S_HEADER_CODE;
-	case S_HEADER_CODE2;
-	case S_HEADER_CODE23;
-	case S_START;
-	case S_CONSTANT;
-	case S_IDENTIFIER;
-	case S_COMMENT;
-}
+$instructions = array("MOVE", "CREATEFRAME", "PUSHFRAME", "POPFRAME", "DEFVAR", "CALL", "RETURN","PUSHS", "POPS",
+					"ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "NOT", "INT2CHAR", "STRI2INT",
+					"READ", "WRITE",
+					"CONCAT", "STRLEN", "GETCHAR", "SETCHAR",
+					"TYPE",
+					"LABEL", "JUMP", "JUMPIFEQ", "JUMPIFNEQ", "EXIT",
+					"DPRINT", "BREAK");
+					
+$commentRegex = "/\s#.*/";
+$singleArgumentRegex = "";
+$twoArgumentRegex = "";
+$threeArgumentRegex = "";
 
-enum TokenType {
-	case CONSTANT;
-	case IDENTIFIER;
-	case LABEL;
-	case GF;
-	case LF;
-	case TF;
-	case END;
+enum LineType {
+	case singleArg;
+	case doubleArg;
+	case tripleArg;
 }
 
 
 class Parser {
 
-
-
+	
 	public function readSTDIN() {
 		$input_data = file_get_contents("php://stdin");
 		return $input_data;
 	}
 
-	public function getNextToken() {
+	public function getType($line) {
 
+	}
+
+	public function getInstructionFromLine($line) {
+		$instruction = explode(' ', trim($line ))[0];
+		return $instruction;
+	}
+
+	public function  getArgumentsFromLine($line) {
+		$argumentsString = substr(strstr($line," "), 1);
+		$arguments = explode(" ", $argumentsString);
+		return $arguments;
+	}
+
+	public function checkHeader($line) {
+		if(strcmp(strtolower($line), strtolower(".IPPcode23"))) {
+			fwrite(STDERR, "Header is either missing or is incorrect");
+			exit(21);
+		}
+	}
+
+	public function createArgument($argument) {
+		$parts = explode("@", $argument);
+		$argType = $parts[0];
+		
+		switch ($argType) {
+			case "GF":
+			case "LF":
+			case "TF":
+			$argument = new Argument("var", $parts[0] . "@" . $parts[1]);
+				break;
+			case "int":
+			$argument = new Argument("int", $parts[1]);
+				break;
+			case "bool":
+			$argument = new Argument("bool", $parts[1]);
+				break;
+			case "string":
+				if(!$parts[1]) {
+					$argument = new Argument("string", "");
+				} else {
+					$argument = new Argument("string", $parts[1]);
+				}
+				break;
+			case "nil":
+				#FIXME: nil values
+				$argument = new Argument("nil", "nil");
+				break;
+			case "type":
+				#TODO:
+				break;
+			
+		}
+		return $argument;
 	}
 
 }
 
-class Token {
+enum ArgumentTypes {
+	case int;
+	case bool;
+	case string;
+	case nil;
+	case label;
+	case type;
+	case var;
+}
+
+class Argument {
 	public $type;
-	public $name;
 	public $value;
-	function __construct($type, $name, $value) {
+
+	function __construct($type, $value) {
 		$this->type = $type;
-		$this->name = $name;
 		$this->value = $value;
 	}
 }
 
 class XML {
-	public function createStartTag($tagName, $args, $value) {
-		$tag = "<". $tagname . " ";
+	public function createTag($tagName, $args, $value) {
+		$tag = "<". $tagName . " ";
 		foreach($args as $arg) {
 			$tag .= $arg . " ";
 		}
 		$tag .= ">" . $value;
 
-		return $tag;
+		echo $tag;
 	}
 
-	public function createEndTag($tagName) {
-		$tag = "</" . $tagname . ">";
-		return $tag;
+	public function endTag($tagName) {
+		$tag = "</" . $tagName . ">";
+		echo $tag . "\n";
+	}
+
+	public function createInstruction($order, $opcode, $args) {
+		$orderString = "order=\"" . $order . "\"";
+		$this->createTag("instruction", [$orderString], "");
+		$this->createTag("opcode", [], $opcode);
+		$this->endTag("opcode");
+		$argCount = 1;
+		foreach($args as $arg) {
+			#FIXME:
+			if($arg->type != "" && $arg->value != ""){
+				$tagName = "arg" . $argCount;
+				$type = "type=\"" . $arg->type . "\"";
+				$this->createTag($tagName, [$type], $arg->value);
+				$this->endTag($tagName);
+				$argCount++;
+			}
+		}
+		$this->endTag("instruction");
 	}
 }
 
@@ -106,6 +176,7 @@ if($argc == 2) {
 }
 
 $parser = new Parser();
+$xml = new XML();
 $input_data = $parser->readSTDIN();
 $lines = explode("\n", $input_data);
 
@@ -115,43 +186,69 @@ $linesWithoutComments = array_map(function($line) {
 
 $noBlankLines = preg_grep('/^[\s]*$/', $linesWithoutComments, PREG_GREP_INVERT);
 
+
+$lineNumber = 0;
+
+echo $XMLHEADER . "\n";
+echo $XMLPROGRAMSTART . "\n";
 foreach($noBlankLines as $line) {
-	echo $line;
-	echo "\n";
+	if($lineNumber == 0) {
+		$parser->checkHeader($line);
+	} else {
+	$instruction = $parser->getInstructionFromLine($line);
+	$arguments = $parser->getArgumentsFromLine($line);
+
+	if(!in_array($instruction, $instructions)) {
+		fwrite("chybne zapsany nebo neznamy operacni kod");
+		exit(22);
+	}
+
+	switch ($instruction) {
+		case "CREATEFRAME":
+		case "PUSHFRAME":
+		case "POPFRAME":
+		case "RETURN":
+		case "BREAK":
+			$xml->createInstruction($lineNumber, $instruction, []);
+			break;
+
+		case "LABEL":
+		case "JUMP":
+			$argument = new Argument("label", $arguments[0]);
+			$xml->createInstruction($lineNumber, $instruction, [$argument]);
+			break;
+
+		default:
+		$argumentList = array();
+		foreach($arguments as $argument) {
+			#echo "***creating argument with: " . $argument . " ***\n";
+			$currentArgument = $parser->createArgument($argument);
+			array_push($argumentList, $currentArgument);
+		}
+		$xml->createInstruction($lineNumber, $instruction, $argumentList);
+		break;
+
+	}
+	}
+	$lineNumber++;
 }
+$xml->endTag("program");
 
 
-if(strcmp(strtolower($noBlankLines[0]), strtolower($HEADER))) {
-	fwrite(STDERR, "Header is either missing or is incorrect");
-	exit(1);
-}
 
-$instructions = array("MOVE", "CREATEFRAME", "PUSHFRAME", "POPFRAME", "DEFVAR", "CALL", "RETURN","PUSHS", "POPS",
-					"ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "NOT", "INT2CHAR", "STRI2INT",
-					"READ", "WRITE",
-					"CONCAT", "STRLEN", "GETCHAR", "SETCHAR",
-					"TYPE",
-					"LABEL", "JUMP", "JUMPIFEQ", "JUMPIFNEQ", "EXIT",
-					"DPRINT", "BREAK");
 
-$commentRegex = "/\s#.*/";
-$singleArgumentRegex = "";
-$twoArgumentRegex = "";
-$threeArgumentRegex = "";
 
 
 
 
 /* 
 plan
-1. parse input into lines
-2. remove comments from lines
-3. remove empty lines
-3. check line if its one of the possible options (instruction/label, ...)
-4. check if the line makes sense in terms of syntax (undefined variable, redefined variable, scope etc)
-5. if line is valid, add it to the output xml
+1. parse input into lines - done
+2. remove comments from lines - done
+3. remove empty lines - done
+4. check if line has valid instruction (valid OP code should be  the first string)
+5. check if the instruction has valid number of arguments
+5. check if the line makes sense in terms of syntax (undefined variable, redefined variable, scope etc)
+6. if line is valid, add it to the output xml, go back to 4.
 */
-
-
-
 ?>
