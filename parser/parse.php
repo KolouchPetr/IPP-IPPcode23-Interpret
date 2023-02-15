65a56d8ddf3eb53cf2b39ba84966ad609d4d623e
KolouchPetr <kolouch.petr4@email.cz>
2023-02-15 09:34:35 +0100

    formating updated

--- parser/parse.php
+++ parser/parse.php
@@ -5,158 +5,201 @@ ini_set('display_errors', 'stderr');
 $XMLHEADER = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
 $XMLPROGRAMSTART = "<program language=\"IPPcode23\">";
 
-$instructions = array("MOVE", "CREATEFRAME", "PUSHFRAME", "POPFRAME", "DEFVAR", "CALL", "RETURN","PUSHS", "POPS",
-					"ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "NOT", "INT2CHAR", "STRI2INT",
-					"READ", "WRITE",
-					"CONCAT", "STRLEN", "GETCHAR", "SETCHAR",
-					"TYPE",
-					"LABEL", "JUMP", "JUMPIFEQ", "JUMPIFNEQ", "EXIT",
-					"DPRINT", "BREAK");
-					
+$instructions = array(
+        "MOVE", "CREATEFRAME", "PUSHFRAME", "POPFRAME", "DEFVAR", "CALL", "RETURN", "PUSHS", "POPS",
+        "ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "NOT", "INT2CHAR", "STRI2INT",
+        "READ", "WRITE",
+        "CONCAT", "STRLEN", "GETCHAR", "SETCHAR",
+        "TYPE",
+        "LABEL", "JUMP", "JUMPIFEQ", "JUMPIFNEQ", "EXIT",
+        "DPRINT", "BREAK"
+);
+
+$instructions = array(
+        "MOVE",
+        "CREATEFRAME",
+        "PUSHFRAME",
+        "POPFRAME",
+        "DEFVAR",
+        "CALL",
+        "RETURN",
+        "PUSHS",
+        "POPS",
+
+        "ADD", "SUB", "MUL", "IDIV", "LT", "GT", "EQ", "AND", "OR", "NOT", "INT2CHAR", "STRI2INT",
+        "READ", "WRITE",
+        "CONCAT", "STRLEN", "GETCHAR", "SETCHAR",
+        "TYPE",
+        "LABEL", "JUMP", "JUMPIFEQ", "JUMPIFNEQ", "EXIT",
+        "DPRINT", "BREAK"
+
+
+
+
+
+
+);
+
 $commentRegex = "/\s#.*/";
 $singleArgumentRegex = "";
 $twoArgumentRegex = "";
 $threeArgumentRegex = "";
 
-enum LineType {
-	case singleArg;
-	case doubleArg;
-	case tripleArg;
+enum LineType
+{
+        case singleArg;
+        case doubleArg;
+        case tripleArg;
 }
 
 #TODO:
 #parse escape sequences
 
-class Parser {
+class Parser
+{
 
-	
-	public function readSTDIN() {
-		$input_data = file_get_contents("php://stdin");
-		return $input_data;
-	}
 
-	public function getType($line) {
+        public function readSTDIN()
+        {
+                $input_data = file_get_contents("php://stdin");
+                return $input_data;
+        }
 
-	}
+        public function getType($line)
+        {
+        }
 
-	public function getInstructionFromLine($line) {
-		$instruction = explode(' ', trim($line ))[0];
-		return $instruction;
-	}
+        public function getInstructionFromLine($line)
+        {
+                $instruction = explode(' ', trim($line))[0];
+                return $instruction;
+        }
 
-	public function  getArgumentsFromLine($line) {
-		$argumentsString = substr(strstr($line," "), 1);
-		$arguments = explode(" ", $argumentsString);
-		return $arguments;
-	}
+        public function  getArgumentsFromLine($line)
+        {
+                $argumentsString = substr(strstr($line, " "), 1);
+                $arguments = explode(" ", $argumentsString);
+                if (count($arguments) > 3) {
+                        fwrite(STDERR, "Too many arguments at line " . $line . " given!");
+                        exit(23);
+                }
+                return $arguments;
+        }
 
-	public function checkHeader($line) {
-		if(strcmp(strtolower($line), strtolower(".IPPcode23"))) {
-			fwrite(STDERR, "Header is either missing or is incorrect");
-			exit(21);
-		}
-	}
+        public function checkHeader($line)
+        {
+                if (strcmp(strtolower($line), strtolower(".IPPcode23"))) {
+                        fwrite(STDERR, "Header is either missing or is incorrect");
+                        exit(21);
+                }
+        }
 
-	public function createArgument($argument) {
-		$parts = explode("@", $argument);
-		$argType = $parts[0];
-		
-		switch ($argType) {
-			case "GF":
-			case "LF":
-			case "TF":
-			$argument = new Argument("var", $parts[0] . "@" . $parts[1]);
-				break;
-			case "int":
-			$argument = new Argument("int", $parts[1]);
-				break;
-			case "bool":
-			$argument = new Argument("bool", $parts[1]);
-				break;
-			case "string":
-				if(!$parts[1]) {
-					$argument = new Argument("string", "");
-				} else {
-					$i = 1;
-					$string = "";
-					while(isset($parts[$i])) {
-						$string .= $parts[$i];
-						$i++;
-					}
-					$argument = new Argument("string", $string);
-				}
-				break;
-			case "nil":
-				#FIXME: nil values
-				$argument = new Argument("nil", "nil");
-				break;
-			case "type":
-				#TODO:
-				break;
-			default:
-				$argument = new Argument("label", $parts[0]);
-				break;
-			
-		}
-		return $argument;
-	}
+        public function createArgument($argument)
+        {
+                $parts = explode("@", $argument);
+                $argType = $parts[0];
 
+                switch ($argType) {
+                        case "GF":
+                        case "LF":
+                        case "TF":
+                                $argument = new Argument("var", $parts[0] . "@" . $parts[1]);
+                                break;
+                        case "int":
+                                $argument = new Argument("int", $parts[1]);
+                                break;
+                        case "bool":
+                                $argument = new Argument("bool", $parts[1]);
+                                break;
+                        case "string":
+                                if (!$parts[1]) {
+                                        $argument = new Argument("string", "");
+                                } else {
+                                        $i = 1;
+                                        $string = "";
+                                        while (isset($parts[$i])) {
+                                                $string .= $parts[$i];
+                                                $i++;
+                                        }
+                                        $argument = new Argument("string", $string);
+                                }
+                                break;
+                        case "nil":
+                                #FIXME: nil values
+                                $argument = new Argument("nil", "nil");
+                                break;
+                        case "type":
+                                #TODO:
+                                break;
+                        default:
+                                $argument = new Argument("label", $parts[0]);
+                                break;
+                }
+                return $argument;
+        }
 }
 
-enum ArgumentTypes {
-	case int;
-	case bool;
-	case string;
-	case nil;
-	case label;
-	case type;
-	case var;
+enum ArgumentTypes
+{
+        case int;
+        case bool;
+        case string;
+        case nil;
+        case label;
+        case type;
+        case var;
 }
 
-class Argument {
-	public $type;
-	public $value;
+class Argument
+{
+        public $type;
+        public $value;
 
-	function __construct($type, $value) {
-		$this->type = $type;
-		$this->value = $value;
-	}
+        function __construct($type, $value)
+        {
+                $this->type = $type;
+                $this->value = $value;
+        }
 }
 
-class XML {
-	public function createTag($tagName, $args, $value) {
-		$tag = "<". $tagName . " ";
-		foreach($args as $arg) {
-			$tag .= $arg . " ";
-		}
-		$tag .= ">" . $value;
+class XML
+{
+        public function createTag($tagName, $args, $value)
+        {
+                $tag = "<" . $tagName . " ";
+                foreach ($args as $arg) {
+                        $tag .= $arg . " ";
+                }
+                $tag .= ">" . $value;
 
-		echo $tag;
-	}
+                echo $tag;
+        }
 
-	public function endTag($tagName) {
-		$tag = "</" . $tagName . ">";
-		echo $tag . "\n";
-	}
+        public function endTag($tagName)
+        {
+                $tag = "</" . $tagName . ">";
+                echo $tag . "\n";
+        }
 
-	public function createInstruction($order, $opcode, $args) {
-		$orderString = "order=\"" . $order . "\"";
-		$opcodeString = "opcode=\"" . $opcode . "\"";
-		$this->createTag("instruction", [$orderString, $opcodeString], "");
-		#$this->createTag("opcode", [], $opcode);
-		#$this->endTag("opcode");
-		$argCount = 1;
-		foreach($args as $arg) {
-			if($arg->type != "" && $arg->value != ""){
-				$tagName = "arg" . $argCount;
-				$type = "type=\"" . $arg->type . "\"";
-				$this->createTag($tagName, [$type], $arg->value);
-				$this->endTag($tagName);
-				$argCount++;
-			}
-		}
-		$this->endTag("instruction");
-	}
+        public function createInstruction($order, $opcode, $args)
+        {
+                $orderString = "order=\"" . $order . "\"";
+                $opcodeString = "opcode=\"" . $opcode . "\"";
+                $this->createTag("instruction", [$orderString, $opcodeString], "");
+                #$this->createTag("opcode", [], $opcode);
+                #$this->endTag("opcode");
+                $argCount = 1;
+                foreach ($args as $arg) {
+                        if ($arg->type != "" && $arg->value != "") {
+                                $tagName = "arg" . $argCount;
+                                $type = "type=\"" . $arg->type . "\"";
+                                $this->createTag($tagName, [$type], $arg->value);
+                                $this->endTag($tagName);
+                                $argCount++;
+                        }
+                }
+                $this->endTag("instruction");
+        }
 }
 
 #TODO:
@@ -182,20 +225,21 @@ class SemanticAnalysis {
 
 */
 
-function printHelp() {
-	echo "TODO: print help";
- }
+function printHelp()
+{
+        echo "TODO: print help";
+}
 
 
 
-if($argc == 2) {
-	if(!strcmp($argv[1], "--help") || !strcmp($argv[1], "-help")) {
-		printHelp();
-		exit(0);
-	} else {
-		echo "use -help or --help to print usage";
-		exit(1);
-	} 
+if ($argc == 2) {
+        if (!strcmp($argv[1], "--help") || !strcmp($argv[1], "-help")) {
+                printHelp();
+                exit(0);
+        } else {
+                echo "use -help or --help to print usage";
+                exit(1);
+        }
 }
 
 $parser = new Parser();
@@ -203,8 +247,8 @@ $xml = new XML();
 $input_data = $parser->readSTDIN();
 $lines = explode("\n", $input_data);
 
-$linesWithoutComments = array_map(function($line) {
-    return preg_replace('/#.*/', '', $line);
+$linesWithoutComments = array_map(function ($line) {
+        return preg_replace('/#.*/', '', $line);
 }, $lines);
 
 $noBlankLines = preg_grep('/^[\s]*$/', $linesWithoutComments, PREG_GREP_INVERT);
@@ -214,49 +258,48 @@ $lineNumber = 0;
 
 echo $XMLHEADER . "\n";
 echo $XMLPROGRAMSTART . "\n";
-foreach($noBlankLines as $line) {
-	if($lineNumber == 0) {
-		$parser->checkHeader($line);
-	} else {
-	$instruction = $parser->getInstructionFromLine($line);
-	$arguments = $parser->getArgumentsFromLine($line);
+foreach ($noBlankLines as $line) {
+        if ($lineNumber == 0) {
+                $parser->checkHeader($line);
+        } else {
+                $instruction = $parser->getInstructionFromLine($line);
+                $arguments = $parser->getArgumentsFromLine($line);
 
-	if(!in_array($instruction, $instructions)) {
-		fwrite("chybne zapsany nebo neznamy operacni kod");
-		exit(22);
-	}
+                if (!in_array($instruction, $instructions)) {
+                        fwrite("chybne zapsany nebo neznamy operacni kod");
+                        exit(22);
+                }
 
-	switch ($instruction) {
-		case "CREATEFRAME":
-		case "PUSHFRAME":
-		case "POPFRAME":
-		case "RETURN":
-		case "BREAK":
-			$xml->createInstruction($lineNumber, $instruction, []);
-			break;
+                switch ($instruction) {
+                        case "CREATEFRAME":
+                        case "PUSHFRAME":
+                        case "POPFRAME":
+                        case "RETURN":
+                        case "BREAK":
+                                $xml->createInstruction($lineNumber, $instruction, []);
+                                break;
 
-		case "LABEL":
-		case "JUMP":
-			$argument = new Argument("label", $arguments[0]);
-			$xml->createInstruction($lineNumber, $instruction, [$argument]);
-			break;
-		#case "JUMPIFEQ":
-		#case "JUMPIFNEQ":
+                        case "LABEL":
+                        case "JUMP":
+                                $argument = new Argument("label", $arguments[0]);
+                                $xml->createInstruction($lineNumber, $instruction, [$argument]);
+                                break;
+                                #case "JUMPIFEQ":
+                                #case "JUMPIFNEQ":
 
 
-		default:
-		$argumentList = array();
-		foreach($arguments as $argument) {
-			#echo "***creating argument with: " . $argument . " ***\n";
-			$currentArgument = $parser->createArgument($argument);
-			array_push($argumentList, $currentArgument);
-		}
-		$xml->createInstruction($lineNumber, $instruction, $argumentList);
-		break;
-
-	}
-	}
-	$lineNumber++;
+                        default:
+                                $argumentList = array();
+                                foreach ($arguments as $argument) {
+                                        #echo "***creating argument with: " . $argument . " ***\n";
+                                        $currentArgument = $parser->createArgument($argument);
+                                        array_push($argumentList, $currentArgument);
+                                }
+                                $xml->createInstruction($lineNumber, $instruction, $argumentList);
+                                break;
+                }
+        }
+        $lineNumber++;
 }
 $xml->endTag("program");
 exit(0);
@@ -268,7 +311,7 @@ exit(0);
 
 
 
-/* 
+/*
 plan
 1. parse input into lines - done
 2. remove comments from lines - done
